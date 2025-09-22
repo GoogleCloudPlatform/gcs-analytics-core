0bd577a9964a4885b874380e556efac5e9861bc5
Ajay Yadav <ajayky@google.com>
2025-09-22 18:58:47 +0000

    checkpoint

--- core/src/jmh/java/com/google/cloud/gcs/analyticscore/core/GoogleCloudStorageInputStreamBenchmark.java
+++ core/src/jmh/java/com/google/cloud/gcs/analyticscore/core/GoogleCloudStorageInputStreamBenchmark.java
@@ -18,15 +18,35 @@ package com.google.cloud.gcs.analyticscore.core;
 
 import org.openjdk.jmh.annotations.*;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
 @State(Scope.Benchmark)
 public class GoogleCloudStorageInputStreamBenchmark {
+//    private static final File TPCDS_CUSTOMER_SF1 = IntegrationTestHelper.getFileFromResources(
+//            "/sampleParquetFiles/tpcds_customer_sf1.parquet");
+//    private static final File TPCDS_CUSTOMER_SF10 = IntegrationTestHelper.getFileFromResources(
+//            "/sampleParquetFiles/tpcds_customer_sf10.parquet");
+//    private static final File TPCDS_CUSTOMER_SF100 = IntegrationTestHelper.getFileFromResources(
+//            "/sampleParquetFiles/tpcds_customer_sf100.parquet");
+
+    @Setup(org.openjdk.jmh.annotations.Level.Invocation)
+    public void uploadSampleFiles() throws IOException {
+        IntegrationTestHelper.uploadFileToGcs(getClass().getResourceAsStream("/sampleParquetFiles/tpcds_customer_sf1.parquet"), "tpcds_customer_sf1.parquet");
+//        IntegrationTestHelper.uploadFileToGcs(TPCDS_CUSTOMER_SF10);
+//        IntegrationTestHelper.uploadFileToGcs(TPCDS_CUSTOMER_SF100);
+    }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
     @Warmup(iterations = 1, time = 1)
     @Measurement(iterations = 2, time = 1)
     @Fork(value = 2, warmups = 1)
-    public void benchmarkMethod() {
-
+    public void parquetFooterParsing_3mbFile_withFooterPrefetchingEnabled() throws IOException {
+        URI uri = IntegrationTestHelper.getGcsObjectUriForFile("tpcds_customer_sf1.parquet");
+        System.out.println(uri);
+        ParquetHelper.readParquetMetadata(uri, true);
     }
 }
