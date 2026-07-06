2459477265c93a9278c0946cbd5c6633a2ecf500
Dheeraj Singh <dheerajsinghh@google.com>
2026-07-06 15:34:45 +0530

    test: add integration tests for GcsFileSystemImpl.create

--- client/src/integrationTest/java/com/google/cloud/gcs/analyticscore/client/GcsFileSystemImplIntegrationTest.java
+++ client/src/integrationTest/java/com/google/cloud/gcs/analyticscore/client/GcsFileSystemImplIntegrationTest.java
@@ -26,6 +26,12 @@ import java.nio.charset.StandardCharsets;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.FileNotFoundException;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.UUID;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
 // TODO: Setup buckets and test data as part of setup on place of relying on existing bucket.
 class GcsFileSystemImplIntegrationTest {
 
@@ -114,5 +120,129 @@ class GcsFileSystemImplIntegrationTest {
                 assertThrows(IOException.class, () -> gcsFileSystem.getFileInfo(URI.create(object)));
 
         assertThat(exception).hasMessageThat().contains("Unable to access blob");
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "gcs.integration.test.bucket", matches = ".+")
+    public void create_object_canWriteContent() throws IOException {
+        // Arrange
+        String bucketName = System.getProperty("gcs.integration.test.bucket");
+        String objectName = "test-folder/test-file-" + UUID.randomUUID() + ".txt";
+        URI uri = URI.create("gs://" + bucketName + "/" + objectName);
+
+        GcsFileSystemOptions options = GcsFileSystemOptions.builder()
+                .setGcsClientOptions(GcsClientOptions.builder().build())
+                .build();
+        GcsFileSystemImpl gcsFileSystem = new GcsFileSystemImpl(options);
+
+        GcsItemId itemId = GcsItemId.builder()
+                .setBucketName(bucketName)
+                .setObjectName(objectName)
+                .build();
+
+        GcsWriteOptions writeOptions = GcsWriteOptions.builder().build();
+        byte[] content = "test content".getBytes(StandardCharsets.UTF_8);
+
+        // Act
+        try (WritableByteChannel channel = gcsFileSystem.create(itemId, writeOptions)) {
+            channel.write(ByteBuffer.wrap(content));
+        }
+
+        // Assert
+        GcsFileInfo fileInfo = gcsFileSystem.getFileInfo(uri);
+        assertThat(fileInfo.getItemInfo().getSize().get()).isEqualTo((long) content.length);
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "gcs.integration.test.bucket", matches = ".+")
+    public void create_overwriteDisabled_throwsFileAlreadyExistsException() throws IOException {
+        // Arrange
+        String bucketName = System.getProperty("gcs.integration.test.bucket");
+        String objectName = "test-folder/test-file-" + UUID.randomUUID() + ".txt";
+
+        GcsFileSystemOptions options = GcsFileSystemOptions.builder()
+                .setGcsClientOptions(GcsClientOptions.builder().build())
+                .build();
+        GcsFileSystemImpl gcsFileSystem = new GcsFileSystemImpl(options);
+
+        GcsItemId itemId = GcsItemId.builder()
+                .setBucketName(bucketName)
+                .setObjectName(objectName)
+                .build();
+
+        // Act & Assert
+        // We do a preliminary setup write (Arrange)
+        GcsWriteOptions writeOptions = GcsWriteOptions.builder().build();
+        try (WritableByteChannel channel = gcsFileSystem.create(itemId, writeOptions)) {
+            channel.write(ByteBuffer.wrap("test".getBytes(StandardCharsets.UTF_8)));
+        }
+
+        GcsWriteOptions noOverwriteOptions = GcsWriteOptions.builder()
+                .setOverwriteExisting(false)
+                .build();
+
+        // Act & Assert
+        assertThrows(FileAlreadyExistsException.class, () -> {
+            gcsFileSystem.create(itemId, noOverwriteOptions);
+        });
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "gcs.integration.test.bucket", matches = ".+")
+    public void create_withParallelCompositeUpload_success() throws IOException {
+        // Arrange
+        String bucketName = System.getProperty("gcs.integration.test.bucket");
+        String objectName = "test-folder/test-file-" + UUID.randomUUID() + ".txt";
+        URI uri = URI.create("gs://" + bucketName + "/" + objectName);
+
+        GcsFileSystemOptions options = GcsFileSystemOptions.builder()
+                .setGcsClientOptions(GcsClientOptions.builder().build())
+                .build();
+        GcsFileSystemImpl gcsFileSystem = new GcsFileSystemImpl(options);
+
+        GcsItemId itemId = GcsItemId.builder()
+                .setBucketName(bucketName)
+                .setObjectName(objectName)
+                .build();
+
+        GcsWriteOptions writeOptions = GcsWriteOptions.builder()
+                .setUploadStrategy(GcsWriteOptions.UploadStrategy.PARALLEL_COMPOSITE_UPLOAD)
+                .build();
+        byte[] content = "test content".getBytes(StandardCharsets.UTF_8);
+
+        // Act
+        try (WritableByteChannel channel = gcsFileSystem.create(itemId, writeOptions)) {
+            channel.write(ByteBuffer.wrap(content));
+        }
+
+        // Assert
+        GcsFileInfo fileInfo = gcsFileSystem.getFileInfo(uri);
+        assertThat(fileInfo.getItemInfo().getSize().get()).isEqualTo((long) content.length);
+    }
+
+    @Test
+    public void create_nonExistentBucket_throwsFileNotFoundException() throws IOException {
+        // Arrange
+        String bucketName = "non-existent-bucket-" + UUID.randomUUID();
+        String objectName = "test-folder/test-file-" + UUID.randomUUID() + ".txt";
+
+        GcsFileSystemOptions options = GcsFileSystemOptions.builder()
+                .setGcsClientOptions(GcsClientOptions.builder().build())
+                .build();
+        GcsFileSystemImpl gcsFileSystem = new GcsFileSystemImpl(options);
+
+        GcsItemId itemId = GcsItemId.builder()
+                .setBucketName(bucketName)
+                .setObjectName(objectName)
+                .build();
+
+        GcsWriteOptions writeOptions = GcsWriteOptions.builder().build();
+
+        // Act & Assert
+        assertThrows(FileNotFoundException.class, () -> {
+            try (WritableByteChannel channel = gcsFileSystem.create(itemId, writeOptions)) {
+                channel.write(ByteBuffer.wrap("test".getBytes(StandardCharsets.UTF_8)));
+            }
+        });
     }
 }
